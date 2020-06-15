4c5f466ba7ec1f8bd5c2716e079f263a9422e61f
phemidawhiz <feminiscence@gmail.com>
2020-06-15 18:01:28 +0100

    Back button navigation

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ns-current-challenge',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./current-challenge.component.css'],
   moduleId: module.id
 })
-export class CurrentChallengeComponent {}
+export class CurrentChallengeComponent {
+
+  constructor(private router: Router) { }
+
+  onTap() {
+    this.router.navigate(['/edit'])
+  }
+}

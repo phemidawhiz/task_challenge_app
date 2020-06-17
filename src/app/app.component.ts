227f80ea36391548ffe1261637579886839710a1
phemidawhiz <feminiscence@gmail.com>
2020-06-17 23:46:28 +0100

    Forwarding the Button is Pressed Info to the App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UIService } from './shared/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ns-app',
   moduleId: module.id,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   activeChallenge = '';
+  private drawerSub: Subscription;
 
-  constructor() {
+  constructor(private uiService: UIService) {
     
+  }
+
+  ngOnInit() {
+    this.drawerSub = this.uiService.drawerState.subscribe();
+    console.log("toggle side drawer");
+  }
+
+  ngOnDestroy() {
+    if(this.drawerSub) {
+      this.drawerSub.unsubscribe();
+    }
   }
 
   onChallengeInput(challengeDescription: string) {

d4a43d036a4b7d5bf6b0c5c2f366bc73bf1d5f50
phemidawhiz <feminiscence@gmail.com>
2020-06-19 20:51:27 +0100

    Passing, reading, and using dynamic parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
 
 import { AppComponent } from './app.component';
 import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
@@ -10,7 +11,6 @@ import { TodayComponent } from './challenges/today/today.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component';
 import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component';
-import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -20,10 +20,11 @@ import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angul
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [ NativeScriptModule, 
-    NativeScriptFormsModule, 
-    AppRoutingModule,
-    NativeScriptUISideDrawerModule
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptUISideDrawerModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-
 export class UIService {
-    private _drawerState = new BehaviorSubject<void>(null);
+  private _drawerState = new BehaviorSubject<void>(null);
 
-    get drawerState() {
-        return this._drawerState.asObservable();
-    }
+  get drawerState() {
+    return this._drawerState.asObservable();
+  }
 
-    public toggleDrawer() {
-        return this._drawerState.next(null);
-    }
+  toggleDrawer() {
+    this._drawerState.next(null);
+  }
 }

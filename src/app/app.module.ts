0254272b444829f1e6d16ac14a1701e5fd783202
phemidawhiz <feminiscence@gmail.com>
2020-06-20 15:08:52 +0100

    implement lazy loading and add missing schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
 
 import { AppComponent } from './app.component';
-import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component';
-import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-edit.component';
 import { AuthComponent } from './auth/auth.component';
-import { TodayComponent } from './challenges/today/today.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component';
-import { ChallengeTabsComponent } from './challenges/challenge-tabs/challenge-tabs.component';
 import { DayModalComponent } from './challenges/day-modal/day-modal.component';
+import { SharedModule } from './shared/shared.module';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -25,16 +21,12 @@ import { DayModalComponent } from './challenges/day-modal/day-modal.component';
     NativeScriptModule,
     NativeScriptFormsModule,
     NativeScriptUISideDrawerModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   declarations: [
     AppComponent,
-    CurrentChallengeComponent,
-    ChallengeEditComponent,
     AuthComponent,
-    TodayComponent,
-    ActionBarComponent,
-    ChallengeTabsComponent,
     DayModalComponent
   ],
   providers: [],

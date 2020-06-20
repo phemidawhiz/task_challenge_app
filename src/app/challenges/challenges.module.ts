1f94b2b416f8aa5259bbdf1d55b11bd6bc3ddeaa
phemidawhiz <feminiscence@gmail.com>
2020-06-20 17:25:13 +0100

    Styling our app

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
 import { ChallengesRoutingModule } from './challenges-routing.module';
@@ -6,6 +6,7 @@ import { ChallengeTabsComponent } from './challenge-tabs/challenge-tabs.componen
 import { CurrentChallengeComponent } from './current-challenge/current-challenge.component';
 import { TodayComponent } from './today/today.component';
 import { SharedModule } from '../shared/shared.module';
+import { ChallengeActionsModule } from './challenge-actions/challenge-actions.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,12 @@ import { SharedModule } from '../shared/shared.module';
     CurrentChallengeComponent,
     TodayComponent
   ],
-  imports: [NativeScriptCommonModule, ChallengesRoutingModule, SharedModule]
+  imports: [
+    NativeScriptCommonModule,
+    ChallengesRoutingModule,
+    SharedModule,
+    ChallengeActionsModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class ChallengesModule {}
